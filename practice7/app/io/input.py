dd9b5ef60461dc7efa7d0635e1dd4f204ed91806
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2025-04-21 02:57:37 +0300

    created template for input and output functions

---
+++ practice7/app/io/input.py
@@ -0,0 +1,11 @@
+def input_console():
+    """Функція для вводу тексту з консолі"""
+    pass
+
+def input_file_builtin(file_path):
+    """Функція для зчитування з файлу за допомогою вбудованих можливостей Python"""
+    pass
+
+def input_file_pandas(file_path):
+    """Функція для зчитування з файлу за допомогою бібліотеки pandas"""
+    pass
---
+++ practice7/app/io/output.py
@@ -0,0 +1,7 @@
+def output_console(text):
+    """Функція для виводу тексту в консоль"""
+    pass
+
+def output_file_builtin(file_path, text):
+    """Функція для запису тексту до файлу за допомогою вбудованих можливостей Python"""
+    pass
